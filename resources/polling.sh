c3c0f7277882e8f58dc2f8c63744a27a596f1a9d
KYLE CRAVIOTTO <KYLE.CRAVIOTTO@LibertyMutual.com>
2022-02-09 21:12:27 -0800

    tweak polling to have a max number of seconds to run

--- resources/polling.sh
+++ resources/polling.sh
@@ -3,10 +3,12 @@
 declare -i duration=1
 declare hasUrl=""
 declare endpoint
+declare maxRuns=60
+declare currentRun=0
 
 usage() {
     cat <<END
-    polling.sh [-i] [-h] endpoint
+    polling.sh [-i] [-h] endpoint secondsToRun
     
     Report the health status of the endpoint
     -i: include Uri for the format
@@ -34,6 +36,8 @@ shift $((OPTIND -1))
 
 if [[ $1 ]]; then
   endpoint=$1
+elif [[ $2 ]]; then
+  maxRuns=$2
 else
   echo "Please specify the endpoint."
   usage
@@ -43,23 +47,32 @@ fi
 
 healthcheck() {
     declare url=$1
-    result=$(curl --http2 -i $url 2>/dev/null | grep "HTTP/[12][12\.]*")
+    result=$(curl -i $url 2>/dev/null | grep "HTTP/1.1")
     echo $result
 }
 
-while [[ true ]]; do
+while [[ true && $currentRun < $maxRuns ]]; do
    result=`healthcheck $endpoint` 
    declare status
    if [[ -z $result ]]; then 
       status="N/A"
+      exit 1
    else
-      status=${result:7:3}
+      status=${result:9:3}
+      if (($status == '200'))
+      then
+        echo "Service is up!"
+        exit 0
+      fi
    fi 
    timestamp=$(date "+%Y%m%d-%H%M%S")
    if [[ -z $hasUrl ]]; then
      echo "$timestamp | $status "
    else
      echo "$timestamp | $status | $endpoint " 
-   fi 
-   sleep $duration
+   fi
+   ((currentRun++))
+   sleep $duration   
 done
+echo "finished running"
+exit 1;
